5d34ec3743264a5871a6c36ed96ee4ee406e870d
Raphael Cohen <raphael.cohen@sekoia.fr>
2020-09-28 15:45:32 +0200

    feat: Fixes in scripts

--- TA-SEKOIA_IO/bin/sekoia_sioc_search.py
+++ TA-SEKOIA_IO/bin/sekoia_sioc_search.py
@@ -7,6 +7,7 @@ import os
 import sys
 import json
 import time
+import traceback
 
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
@@ -14,15 +15,12 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 
 # third parties
 import splunk
-from splunklib.searchcommands import (
-    dispatch,
-    StreamingCommand,
-    Configuration
-)
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration
 from splunklib import client
 
 # internals
 from utils import setup_logging
+
 
 class NoSplunkPatternFoundError(Exception):
     pass
@@ -30,7 +28,6 @@ class NoSplunkPatternFoundError(Exception):
 
 @Configuration()
 class SIOCSearchCommand(StreamingCommand):
-
     def get_session_key(self):
         return self._metadata.searchinfo.session_key
 
@@ -40,41 +37,42 @@ class SIOCSearchCommand(StreamingCommand):
             host=splunk.getDefault("host"),
             port=splunk.getDefault("port"),
             app="TA-SEKOIA_IO",
-            owner="nobody"
+            owner="nobody",
         )
-    
-    def generate_search_job(self, record):
 
+    def generate_search_job(self, record):
         try:
-            indicator = record
             service = self.get_api_service()
-            searchquery = indicator.get("x_pattern_splunk")
-            if searchquery is None:
+            searchquery = record.get("x_pattern_splunk")
+            if not searchquery:
                 raise NoSplunkPatternFoundError()
 
             # inject the index filtering in the query
             searchquery = searchquery.replace(" earliest=", " index=test earliest=")
 
             # extend the search query to report the result in kvstore
-            searchquery += ', index | stats values(index) as match_indexes earliest(_time) as match_first_seen latest(_time) as match_last_seen count as match_count | eval indicator_id="'+str(indicator["id"])+'" |  lookup sioc_lookup indicator_id AS indicator_id OUTPUTNEW | outputlookup sioc_lookup append=True key_field=_key max=1'            
-            kwargs_search = {"exec_mode": "normal"}
-            
-            job = service.jobs.create(
-                searchquery, **kwargs_search
+            searchquery += (
+                ', index | stats values(index) as match_indexes earliest(_time) as match_first_seen latest(_time) as match_last_seen count as match_count | eval indicator_id="'
+                + str(record["id"])
+                + '" |  lookup sioc_lookup indicator_id AS indicator_id OUTPUTNEW | outputlookup sioc_lookup append=True key_field=_key max=1'
             )
+            kwargs_search = {"exec_mode": "normal"}
+
+            job = service.jobs.create(searchquery, **kwargs_search)
 
             # store job_id in kv store
-            self.report_search(job_id=job.name, indicator=indicator)                
+            self.report_search(job_id=job.name, record=record)
 
         except NoSplunkPatternFoundError:
             raise
-        
+
         except Exception as e:
             self._logger.error(e)
             return "failed"
         return job.name
 
-    def report_search(self, job_id, indicator):
+    def report_search(self, job_id, record):
+        indicator = json.loads(record["_raw"])
         service = self.get_api_service()
         collection_name = "sioc_search_jobs"
         if collection_name not in service.kvstore:
@@ -90,35 +88,40 @@ class SIOCSearchCommand(StreamingCommand):
             "match_first_seen": "",
             "match_last_seen": "",
             "match_indexes": "",
-            "indicator_id": indicator['id'],
-            "indicator_created": indicator.get('created'),
-            "indicator_created_by_ref": indicator.get('created_by_ref'),
-            "indicator_indicator_types": indicator.get('indicator_types'),
+            "indicator_id": indicator["id"],
+            "indicator_created": indicator.get("created"),
+            "indicator_created_by_ref": indicator.get("created_by_ref"),
+            "indicator_indicator_types": indicator.get("indicator_types"),
             "indicator_kill_chain_phases": [
-                str(killchain.get('kill_chain_name'))+':'+str(killchain.get('phase_name')) for killchain in indicator.get('kill_chain_phases', [])
+                str(killchain.get("kill_chain_name")) + ":" + str(killchain.get("phase_name"))
+                for killchain in indicator.get("kill_chain_phases", [])
             ],
             "indicator_name": indicator.get("name"),
             "indicator_pattern": indicator.get("pattern"),
             "indicator_pattern_type": indicator.get("pattern_type"),
             "indicator_valid_from": indicator.get("valid_from"),
             "indicator_valid_until": indicator.get("valid_until"),
-            "indicator_x_pattern_splunk": indicator.get("x_pattern_splunk"),
-            "indicator_x_inthreat_sources_refs": indicator.get("x_inthreat_sources_refs")            
+            "indicator_x_pattern_splunk": record.get("x_pattern_splunk"),
+            "indicator_x_inthreat_sources_refs": indicator.get("x_inthreat_sources_refs"),
         }
-        
-        res = collection.data.insert(json.dumps(kv_fields))            
-        self._logger.info('Search report inserted in collection '+str(collection_name)+': '+str(res))
-        
-    def stream(self, records):
 
+        res = collection.data.insert(json.dumps(kv_fields))
+        self._logger.info(
+            "Search report inserted in collection " + str(collection_name) + ": " + str(res)
+        )
+
+    def stream(self, records):
         self._logger = setup_logging("ta_sekoiaio_siocsearch")
 
         # max number of search per run
         max_search_per_run = 50
         nb_triggered_search = 0
+
         for record in records:
             if nb_triggered_search >= max_search_per_run:
-                self._logger.warn("Max number of search per run reached, other searches will be processed in next run")
+                self._logger.warn(
+                    "Max number of search per run reached, other searches will be processed in next run"
+                )
             else:
                 try:
                     job_id = self.generate_search_job(record)
@@ -126,8 +129,9 @@ class SIOCSearchCommand(StreamingCommand):
                     nb_triggered_search += 1
                 except NoSplunkPatternFoundError:
                     record["job_id"] = "no pattern"
-                
+
             yield record
+
 
 if __name__ == "__main__":
     dispatch(SIOCSearchCommand, sys.argv, sys.stdin, sys.stdout, __name__)
