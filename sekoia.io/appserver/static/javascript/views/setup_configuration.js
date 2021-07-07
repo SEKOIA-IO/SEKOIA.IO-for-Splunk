53f393aca60464a6eba595f043edeecd262c72ef
Gael Muller <muller.gael@gmail.com>
2021-07-07 11:23:16 +0200

    fix "NOT FOUND" error

--- sekoia.io/appserver/static/javascript/views/setup_configuration.js
+++ sekoia.io/appserver/static/javascript/views/setup_configuration.js
@@ -1,48 +1,35 @@
-import * as SplunkHelpers from './splunk_helpers.js'
-import { APP_NAME } from "./constants.js"
+import * as SplunkHelpers from "./splunk_helpers.js";
+import { APP_NAME } from "./constants.js";
 
-async function setup_modular_input(
-  splunk_js_sdk_service,
-  properties
-) {
+async function setup_modular_input(splunk_js_sdk_service, properties) {
   await SplunkHelpers.update_configuration_file(
     splunk_js_sdk_service,
     "inputs",
-    "sekoia_indicators://SEKOIA.IO Intelligence Center",
+    "sekoia_indicators://feed",
     properties
   );
 }
 
-async function get_input_settings(
-  splunk_js_sdk_service
-) {
-  return await SplunkHelpers.get_configuration(
-    splunk_js_sdk_service,
-    "inputs",
-  );
+async function get_input_settings(splunk_js_sdk_service) {
+  return await SplunkHelpers.get_configuration(splunk_js_sdk_service, "inputs");
 }
 
-async function get_lookup_settings(
-  splunk_js_sdk_service
-) {
+async function get_lookup_settings(splunk_js_sdk_service) {
   return await SplunkHelpers.get_configuration(
     splunk_js_sdk_service,
     "savedsearches"
   );
 }
 
-async function setup_lookups(
-  splunk_js_sdk_service,
-  lookups
-) {
+async function setup_lookups(splunk_js_sdk_service, lookups) {
   const CRON_SCHEDULES = {
-    "ipv4": "10 * * * *",
-    "domain": "15 * * * *",
-    "url": "20 * * * *",
-    "md5": "25 * * * *",
-    "sha1": "30 * * * *",
-    "sha256": "35 * * * *"
-  }
+    ipv4: "10 * * * *",
+    domain: "15 * * * *",
+    url: "20 * * * *",
+    md5: "25 * * * *",
+    sha1: "30 * * * *",
+    sha256: "35 * * * *",
+  };
 
   var searches = splunk_js_sdk_service.savedSearches();
   await searches.fetch();
@@ -62,9 +49,9 @@ async function setup_lookups(
       search: `${lookups[index].query} | lookup sekoia_iocs_${lookups[index].type} _key as ${lookups[index].field} OUTPUTNEW _key as matched_ioc indicator_id as indicator_id | search matched_ioc=* | eval event=_raw, event_time=_time, sighting_hash=sha256(host.index.sourcetype.event), ioc_type="${lookups[index].type}" | fields event_time,matched_ioc,ioc_type,indicator_id,host,index,sourcetype,event,sighting_hash | outputlookup sekoia_alerts append=true key_field=sighting_hash`,
       "dispatch.earliest_time": "-65m@m",
       "dispatch.latest_time": "-5m@m",
-      "is_scheduled": 1,
-      "cron_schedule": CRON_SCHEDULES[lookups[index].type],
-    }
+      is_scheduled: 1,
+      cron_schedule: CRON_SCHEDULES[lookups[index].type],
+    };
 
     searches.create(lookup);
   }
@@ -72,13 +59,13 @@ async function setup_lookups(
 
 async function setup_cleanup(splunk_js_sdk_service) {
   const CRON_SCHEDULES = {
-    "ipv4": "40 2 * * *",
-    "domain": "45 2 * * *",
-    "url": "50 2 * * *",
-    "md5": "55 2 * * *",
-    "sha1": "0 2 * * *",
-    "sha256": "5 2 * * *"
-  }
+    ipv4: "40 2 * * *",
+    domain: "45 2 * * *",
+    url: "50 2 * * *",
+    md5: "55 2 * * *",
+    sha1: "0 2 * * *",
+    sha256: "5 2 * * *",
+  };
   const ioc_types = Object.keys(CRON_SCHEDULES);
 
   var searches = splunk_js_sdk_service.savedSearches();
@@ -89,7 +76,7 @@ async function setup_cleanup(splunk_js_sdk_service) {
       name: `SEKOIA.IO IOC Cleanup - ${ioc_types[index]}`,
       search: `| inputlookup sekoia_iocs_${ioc_types[index]} | convert num(valid_until) | where isNull(valid_until) OR valid_until > now() | outputlookup sekoia_iocs_${ioc_types[index]}`,
       is_scheduled: 1,
-      cron_schedule: CRON_SCHEDULES[ioc_types[index]]
+      cron_schedule: CRON_SCHEDULES[ioc_types[index]],
     };
 
     searches.create(cleanup);
@@ -98,70 +85,61 @@ async function setup_cleanup(splunk_js_sdk_service) {
 
 async function create_custom_configuration_file(
   splunk_js_sdk_service,
-  api_url,
+  api_url
 ) {
   var custom_configuration_file_name = "setup_page_example";
   var stanza_name = "example_stanza";
   var properties_to_update = {
-      api_url: api_url,
+    api_url: api_url,
   };
 
   await SplunkHelpers.update_configuration_file(
-      splunk_js_sdk_service,
-      custom_configuration_file_name,
-      stanza_name,
-      properties_to_update,
+    splunk_js_sdk_service,
+    custom_configuration_file_name,
+    stanza_name,
+    properties_to_update
   );
-};
+}
 
 async function complete_setup(splunk_js_sdk_service) {
   var configuration_file_name = "app";
   var stanza_name = "install";
   var properties_to_update = {
-      is_configured: "true",
+    is_configured: "true",
   };
 
   await SplunkHelpers.update_configuration_file(
-      splunk_js_sdk_service,
-      configuration_file_name,
-      stanza_name,
-      properties_to_update,
+    splunk_js_sdk_service,
+    configuration_file_name,
+    stanza_name,
+    properties_to_update
   );
-};
+}
 
-async function reload_splunk_app(
-  splunk_js_sdk_service,
-  app_name,
-) {
+async function reload_splunk_app(splunk_js_sdk_service, app_name) {
   var splunk_js_sdk_apps = splunk_js_sdk_service.apps();
   await splunk_js_sdk_apps.fetch();
 
   var current_app = splunk_js_sdk_apps.item(app_name);
   current_app.reload();
-};
+}
 
-function redirect_to_splunk_app_homepage(
-  app_name,
-) {
+function redirect_to_splunk_app_homepage(app_name) {
   var redirect_url = "/app/" + app_name;
 
   window.location.href = redirect_url;
-};
+}
 
-
-function create_splunk_js_sdk_service(
-  splunk_js_sdk,
-  application_name_space,
-) {
+function create_splunk_js_sdk_service(splunk_js_sdk, application_name_space) {
   var http = new splunk_js_sdk.SplunkWebHttp();
 
   var splunk_js_sdk_service = new splunk_js_sdk.Service(
-      http,
-      application_name_space,
+    http,
+    application_name_space
   );
 
   return splunk_js_sdk_service;
-};
+}
 
 export {
   create_custom_configuration_file,
@@ -173,5 +151,5 @@ export {
   get_input_settings,
   get_lookup_settings,
   setup_lookups,
-  setup_cleanup
-}
+  setup_cleanup,
+};
